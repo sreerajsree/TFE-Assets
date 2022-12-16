ca0bb9bd3a80b10948330fee0f458c781cd9d2bd
sreerajsree <sreeandsreeraj@gmail.com>
2022-12-16 12:51:53 +0530

    first commit

---
+++ backend/js/page/modules-datatables.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+$("#table-1").dataTable({
+  "columnDefs": [
+    { "sortable": false, "targets": [2,3] }
+  ]
+});
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "The Fashion Enthusiast Assets";
+
+?>
